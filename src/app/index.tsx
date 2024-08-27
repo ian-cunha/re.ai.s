90ec939d21800ce87376bbc8dff8711e5b27a6ce
Ian <iancunha20@gmail.com>
2024-08-27 00:23:14 -0300

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,43 +1,53 @@
 import { WebView } from 'react-native-webview';
 import Constants from 'expo-constants';
 import { StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Index() {
 
+  const[uri, seturi] = useState('https://app.smartimobiliario.com.br/atendimento/negocios')
+
   return (
-      <WebView
-        style={styles.container}
-        source={{ uri: 'https://app.smartimobiliario.com.br/atendimento/negocios' }}
-        /*
-        onMessage={() => {}}
-        ref={() => {}}
-        */
-        javaScriptEnabled={true}
-        setSupportMultipleWindows={false}
-        startInLoadingState={true}
-        mediaPlaybackRequiresUserAction={false}
-        allowFileAccess={true}
-        allowFileAccessFromFileURLs={true}
-        allowUniversalAccessFromFileURLs={true}
-        domStorageEnabled={true}
-        allowsBackForwardNavigationGestures
-        useWebKit={true}
-        cacheEnabled={true}
-        sharedCookiesEnabled={true}
-        thirdPartyCookiesEnabled={true}
-        pullToRefreshEnabled={true}
-        showsHorizontalScrollIndicator={false}
-        showsVerticalScrollIndicator={false}
-        mediaCapturePermissionGrantType="grantIfSameHostElsePrompt"
-        scalesPageToFit={false}
-        userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-        cacheMode="LOAD_CACHE_ELSE_NETWORK"
-        originWhitelist={['*']}
-        mixedContentMode="always"
-        incognito={false}
-        geolocationEnabled={true}
-      />
+    <WebView
+      style={styles.container}
+      source={{ uri }}
+      onShouldStartLoadWithRequest={
+        request => {
+          if (request.url.includes('https')) {
+              seturi(request.url);
+              return false;
+          } else return true;
+         }
+      }
+      /*
+      onMessage={() => {}}
+      ref={() => {}}
+      */
+      javaScriptEnabled={true}
+      setSupportMultipleWindows={false}
+      startInLoadingState={true}
+      mediaPlaybackRequiresUserAction={false}
+      allowFileAccess={true}
+      allowFileAccessFromFileURLs={true}
+      allowUniversalAccessFromFileURLs={true}
+      domStorageEnabled={true}
+      allowsBackForwardNavigationGestures
+      useWebKit={true}
+      cacheEnabled={true}
+      sharedCookiesEnabled={true}
+      thirdPartyCookiesEnabled={true}
+      pullToRefreshEnabled={true}
+      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}
+      mediaCapturePermissionGrantType="grantIfSameHostElsePrompt"
+      scalesPageToFit={false}
+      userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+      cacheMode="LOAD_CACHE_ELSE_NETWORK"
+      originWhitelist={['*']}
+      mixedContentMode="always"
+      incognito={false}
+      geolocationEnabled={true}
+    />
   );
 }
 
