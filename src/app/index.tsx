7f19cd164f4e915604bcc91f85fb54d06ce7d27c
Ian <iancunha20@gmail.com>
2024-09-27 23:00:35 -0300

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,61 +1,101 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, BackHandler } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, BackHandler, Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { WebView } from 'react-native-webview';
 import Constants from 'expo-constants';
 
 const Index: React.FC = () => {
   const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [stayLoggedIn, setStayLoggedIn] = useState<boolean>(false);
-  const [loggedIn, setLoggedIn] = useState<boolean>(false);
-  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+  const [senha, setSenha] = useState<string>('');
+  const [manterLogado, setManterLogado] = useState<boolean>(false);
+  const [logado, setLogado] = useState<boolean>(false);
+  const [senhaVisivel, setSenhaVisivel] = useState<boolean>(false);
+  const [urlWebView, setUrlWebView] = useState<string | null>(null);
 
   const webViewRef = useRef<WebView | null>(null);
 
-  const loginUrl = 'https://app.smartimobiliario.com.br/';
-  const logoutUrl = 'https://app.smartimobiliario.com.br/usuario/logout';
-
-  const injectJS = `
-    document.getElementById('email').value = '${email}';
-    document.getElementById('senha').value = '${password}';
-    document.forms[0].submit();
-  `;
+  const urlRedirect = 'https://app.smartimobiliario.com.br/inicio/dashboard?origem=login';
+  const urlLogin = 'https://app.smartimobiliario.com.br/usuario/loginSmart';
+  const urlLogout = 'https://app.smartimobiliario.com.br/usuario/logout';
 
   useEffect(() => {
-    const checkLoggedIn = async () => {
-      const storedEmail = await AsyncStorage.getItem('email');
-      const storedPassword = await AsyncStorage.getItem('senha');
-      if (storedEmail && storedPassword) {
-        setEmail(storedEmail);
-        setPassword(storedPassword);
-        setLoggedIn(true);
+    const verificarLogado = async () => {
+      const emailArmazenado = await AsyncStorage.getItem('email');
+      const senhaArmazenada = await AsyncStorage.getItem('senha');
+      if (emailArmazenado && senhaArmazenada) {
+        setEmail(emailArmazenado);
+        setSenha(senhaArmazenada);
+        setLogado(true);
+        setUrlWebView(urlLogin);
       }
     };
-    checkLoggedIn();
+
+    verificarLogado();
   }, []);
 
   const handleLogin = async () => {
-    if (stayLoggedIn) {
-      await AsyncStorage.setItem('email', email);
-      await AsyncStorage.setItem('senha', password);
-    } else {
-      await AsyncStorage.removeItem('email');
-      await AsyncStorage.removeItem('senha');
+    const tipoDispositivo = Platform.OS === 'ios' ? 'iphone' : 'android';
+
+    const body = new URLSearchParams({
+      'usuario.email': email,
+      'usuario.senha': senha,
+      urlRedirect: urlRedirect,
+      tipoDispositivo: tipoDispositivo,
+      tokenNotificacao: '',
+    }).toString();
+
+    try {
+      const resposta = await fetch(urlLogin, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: body,
+      });
+
+      if (!resposta.ok) {
+        const errorText = await resposta.text();
+        console.error('Erro na requisição:', errorText);
+        alert('Erro na requisição. Verifique os dados e tente novamente.');
+        return;
+      }
+
+      const dados = await resposta.json();
+      console.log('Status da resposta do login:', resposta.status);
+      console.log('Dados da resposta do login:', dados);
+
+      if (dados.success) {
+        if (manterLogado) {
+          await AsyncStorage.setItem('email', email);
+          await AsyncStorage.setItem('senha', senha);
+        } else {
+          await AsyncStorage.removeItem('email');
+          await AsyncStorage.removeItem('senha');
+        }
+        setLogado(true);
+        setUrlWebView(urlLogin);
+      } else {
+        console.log('Login falhou:', dados.message || 'Verifique suas credenciais.');
+        alert('Login falhou: ' + (dados.message || 'Verifique suas credenciais.'));
+      }
+    } catch (error) {
+      console.error('Ocorreu um erro durante o login:', error);
+      alert('Ocorreu um erro. Tente novamente.');
     }
-    setLoggedIn(true);
   };
+
 
   const handleLogout = async () => {
     await AsyncStorage.removeItem('email');
     await AsyncStorage.removeItem('senha');
-    setLoggedIn(false);
+    setLogado(false);
     setEmail('');
-    setPassword('');
+    setSenha('');
+    setUrlWebView(null);
   };
 
   const handleNavigationChange = (navState: any) => {
-    if (navState.url.includes(logoutUrl)) {
+    if (navState.url.includes(urlLogout)) {
       handleLogout();
     }
   };
@@ -64,9 +104,9 @@ const Index: React.FC = () => {
     const backAction = () => {
       if (webViewRef.current) {
         webViewRef.current.goBack();
-        return true; // impede o fechamento do aplicativo
+        return true;
       }
-      return false; // permite o fechamento se não houver histórico no WebView
+      return false;
     };
 
     const backHandler = BackHandler.addEventListener(
@@ -79,7 +119,7 @@ const Index: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      {!loggedIn ? (
+      {!logado ? (
         <View style={styles.loginContainer}>
           <Text style={styles.title}>Smart Imobiliário</Text>
           <Text style={styles.subtitle}>Faça login na sua conta</Text>
@@ -97,18 +137,18 @@ const Index: React.FC = () => {
           <View style={styles.passwordContainer}>
             <TextInput
               style={styles.inputPassword}
-              value={password}
-              onChangeText={setPassword}
+              value={senha}
+              onChangeText={setSenha}
               placeholder="Digite sua senha"
-              secureTextEntry={!passwordVisible}
+              secureTextEntry={!senhaVisivel}
               placeholderTextColor="#bbb"
             />
             <TouchableOpacity
               style={styles.toggleButton}
-              onPress={() => setPasswordVisible(!passwordVisible)}
+              onPress={() => setSenhaVisivel(!senhaVisivel)}
             >
               <Text style={styles.toggleText}>
-                {passwordVisible ? 'Ocultar' : 'Mostrar'}
+                {senhaVisivel ? 'Ocultar' : 'Mostrar'}
               </Text>
             </TouchableOpacity>
           </View>
@@ -116,10 +156,10 @@ const Index: React.FC = () => {
           <View style={styles.stayLoggedInContainer}>
             <TouchableOpacity
               style={styles.checkboxContainer}
-              onPress={() => setStayLoggedIn(!stayLoggedIn)}
+              onPress={() => setManterLogado(!manterLogado)}
             >
-              <View style={[styles.checkbox, stayLoggedIn && styles.checked]}>
-                {stayLoggedIn && <Text style={styles.checkmark}>✓</Text>}
+              <View style={[styles.checkbox, manterLogado && styles.checked]}>
+                {manterLogado && <Text style={styles.checkmark}>✓</Text>}
               </View>
               <Text style={styles.stayLoggedInText}>Permanecer logado</Text>
             </TouchableOpacity>
@@ -134,35 +174,20 @@ const Index: React.FC = () => {
           <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
             <Text style={styles.logoutButtonText}>Sair</Text>
           </TouchableOpacity>
-          <WebView
-            source={{ uri: loginUrl }}
-            ref={webViewRef}
-            onLoadEnd={() => {
-              if (loggedIn && webViewRef.current) {
-                webViewRef.current.injectJavaScript(injectJS);
-              }
-            }}
-            onNavigationStateChange={handleNavigationChange}
-            javaScriptEnabled={true}
-            setSupportMultipleWindows={false}
-            mediaPlaybackRequiresUserAction={false}
-            allowFileAccess={true}
-            allowFileAccessFromFileURLs={true}
-            allowUniversalAccessFromFileURLs={true}
-            javaScriptCanOpenWindowsAutomatically={true}
-            domStorageEnabled={true}
-            allowsBackForwardNavigationGestures
-            cacheEnabled={true}
-            sharedCookiesEnabled={true}
-            thirdPartyCookiesEnabled={true}
-            pullToRefreshEnabled={true}
-            showsHorizontalScrollIndicator={false}
-            showsVerticalScrollIndicator={false}
-            mediaCapturePermissionGrantType="grantIfSameHostElsePrompt"
-            scalesPageToFit={false}
-            mixedContentMode="always"
-            geolocationEnabled={true}
-          />
+          {urlWebView && (
+            <WebView
+              source={{ uri: urlWebView }}
+              ref={webViewRef}
+              onNavigationStateChange={handleNavigationChange}
+              javaScriptEnabled={true}
+              allowsBackForwardNavigationGestures
+              cacheEnabled={true}
+              sharedCookiesEnabled={true}
+              pullToRefreshEnabled={true}
+              showsHorizontalScrollIndicator={false}
+              showsVerticalScrollIndicator={false}
+            />
+          )}
         </>
       )}
     </View>
